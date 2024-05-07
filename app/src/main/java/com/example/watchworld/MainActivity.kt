bb163d6de25a1add8986cf602f935d3832ca2882
Valentijn <valentijnkeijser@gmail.com>
2024-05-07 23:25:21 +0200

    working wrong ui

--- app/src/main/java/com/example/watchworld/MainActivity.kt
+++ app/src/main/java/com/example/watchworld/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.watchworld
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.watchworld.ui.theme.WatchWorldTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WebPage("https://spiley.pythonanywhere.com/")
                 }
             }
         }
@@ -30,17 +34,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun WebPage(url: String) {
+    AndroidView(
+//        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                // Load the website URL
+                loadUrl(url)
+            }
+        }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WatchWorldTheme {
-        Greeting("Android")
-    }
 }
